cfdf1b73f3d95c9af5f87077ad58e2fd11d69c03
Beerendan <brendanrbowen@gmail.com>
2023-06-22 12:34:42 -0400

    layout designation

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+let search="";
+let city="";
+let weather="";
+let current="";
+let lat="";
+let long="";
+
+let local=window.localStorage;
+
+//searched cities and its holder
+let searched=[];
+let holder=[];
+
+//API keys
+const weatherKey= "460fae23bd2d7430796b3dce400149fc";
+const geoKey="VlqjssRrcUenmcGjbpPDJxdS6INYZ30G";
+
+//On page load
+reset();
+reload();
+
+function reset(){
+    $("#")
+}
